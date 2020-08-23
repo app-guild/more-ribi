688e56e8abcca184ad01a7b0026d57c61dbf9bf6
Max <max.dobro@mail.ru>
2020-08-23 18:25:51 +0300

    navigation

---
+++ src/screens/MyOrdersScreen.tsx
@@ -0,0 +1,25 @@
+import React, {Component} from "react";
+import {Text, View} from "react-native";
+import {stylesheet} from "../../resources/styles";
+
+export interface IMyOrdersScreenState {}
+
+class MyOrdersScreen extends Component<
+  Readonly<any>,
+  Readonly<IMyOrdersScreenState>
+  > {
+  constructor(props: any) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <View style={stylesheet.centerBody}>
+        <Text>My Orders Screen</Text>
+      </View>
+    );
+  }
+}
+
+export default MyOrdersScreen;
