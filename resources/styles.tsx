8b014c2ebe9d042d235be7ec9c21d64d971c5b21
Max <max.dobro@mail.ru>
2020-08-23 20:01:16 +0300

    main screen

--- resources/styles.tsx
+++ resources/styles.tsx
@@ -60,6 +60,100 @@ const stylesheet = StyleSheet.create({
         backgroundColor: globals.navigatorBackgroundColor,
         paddingBottom: 10,
     },
+    headerContainer: {
+        paddingVertical: 8,
+        paddingHorizontal: 12,
+    },
+    headerTitle: {
+        fontFamily: "Montserrat",
+        fontStyle: "normal",
+        fontWeight: "900",
+        fontSize: 24,
+        lineHeight: 29,
+        color: globals.primaryColor,
+        marginLeft: 9,
+    },
+    headerSubTitle: {
+        fontFamily: "Muli",
+        fontStyle: "normal",
+        fontWeight: "bold",
+        fontSize: 16,
+        lineHeight: 20,
+        color: globals.primaryColor,
+        marginRight: 7,
+    },
+    headerTopContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    headerSubContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+    headerPriceText: {
+        fontFamily: "Muli",
+        fontStyle: "normal",
+        fontWeight: "bold",
+        fontSize: 14,
+        lineHeight: 18,
+        color: globals.primaryColor,
+        marginLeft: 6,
+    },
+    dishCardContainer: {
+        borderRadius: 20,
+        padding: 10,
+        backgroundColor: "#FFFFFF",
+
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.20,
+        shadowRadius: 1.41,
+        elevation: 2,
+    },
+    dishCardName: {
+        fontFamily: "Muli",
+        fontStyle: "normal",
+        fontWeight: "bold",
+        fontSize: 14,
+        lineHeight: 18,
+        color: globals.mainTextColor,
+        paddingTop: 5,
+    },
+    dishCardComposition: {
+        fontFamily: "Muli",
+        fontStyle: "normal",
+        fontWeight: "300",
+        fontSize: 10,
+        lineHeight: 13,
+        color: "#CFD2D7",
+    },
+    dishCardPrice: {
+        fontFamily: "Muli",
+        fontStyle: "normal",
+        fontWeight: "normal",
+        fontSize: 10,
+        lineHeight: 13,
+        color: globals.primaryColor,
+        //textAlign: "right",
+        paddingTop: 13,
+    },
+    dishCardShoppingCartButton:{
+        borderRadius: 50,
+        backgroundColor: "#000000",
+        width: 30,
+        height: 30,
+        opacity: 0.2
+    },
+    dishCardShoppingCartButtonContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    mainScreenContainer: {
+        width: "100%",
+    },
 });
 
 export {stylesheet, globals};
--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,25 +1,95 @@
 import React, {Component} from "react";
-import {Text, View} from "react-native";
+import {FlatList, Dimensions} from "react-native";
 import {stylesheet} from "../../resources/styles";
+import Header from "../components/Header";
+import DishCard from "../components/DishCard";
 
-export interface IMainScreenState {}
 
-class MainScreen extends Component<
-  Readonly<any>,
-  Readonly<IMainScreenState>
-  > {
-    constructor(props: any) {
-        super(props);
-        this.state = {};
-    }
+export interface IMainScreenState {
+  mainContainerWidth: number,
+  cardsInfo: any[],
+}
 
-    render() {
-        return (
-          <View style={stylesheet.centerBody}>
-              <Text>Main Screen</Text>
-          </View>
-        );
-    }
+class MainScreen extends Component<Readonly<any>, Readonly<IMainScreenState>> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      mainContainerWidth: Dimensions.get("window").width,
+      cardsInfo: [
+        {
+          id: 0,
+          name: "Поке с лососем",
+          composition: "Рис, лосось, авокадо, красный лук, салат, морковь",
+          price: 290,
+        },
+        {
+          id: 1,
+          name: "Поке с лососем 2",
+          composition: "Рис, лосось, авокадо, красный лук, салат, морковь",
+          price: 290,
+        },
+        {
+          id: 2,
+          name: "Поке с лососем 3",
+          composition: "Рис, лосось, авокадо, красный лук, салат, морковь",
+          price: 290,
+        },
+        {
+          id: 3,
+          name: "Поке с лососем 4",
+          composition: "Рис, лосось, авокадо, красный лук, салат, морковь",
+          price: 290,
+        },
+        {
+          id: 4,
+          name: "Поке с лососем 5",
+          composition: "Рис, лосось, авокадо, красный лук, салат, морковь",
+          price: 290,
+        },
+        {
+          id: 5,
+          name: "Поке с лососем 6",
+          composition: "Рис, лосось, авокадо, красный лук, салат, морковь",
+          price: 290,
+        },
+      ]
+    };
+  }
+
+  render() {
+    // @ts-ignore
+    const renderItem = ({ item, index }) => (
+      <DishCard
+        width={(this.state.mainContainerWidth - 2*27 - 17)/2}
+        name={item.name}
+        composition={item.composition}
+        price={item.price}
+        style={{
+          marginLeft: !(index%2) ? 27 : 17,
+          marginRight: !(index%2) ? 0 : 27,
+          marginTop: index > 1 ? 0 : 6,
+          marginBottom: index > this.state.cardsInfo.length-3 ? 14 : 20,
+        }}
+
+      />
+    );
+
+    return (
+      <>
+        <Header
+          navigation={this.props.navigation}
+        />
+
+        <FlatList
+          data={this.state.cardsInfo}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+          numColumns={2}
+          style={stylesheet.mainScreenContainer}
+        />
+      </>
+    );
+  }
 }
 
 export default MainScreen;
