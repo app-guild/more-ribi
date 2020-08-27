858ddb5932c5672752882650e2e99a173dca04e8
Max <max.dobro@mail.ru>
2020-08-27 23:26:46 +0300

    categories screen

---
+++ src/components/CategoryCard.tsx
@@ -0,0 +1,51 @@
+import React, {Component} from "react";
+import {Image, Text, View} from "react-native";
+import {stylesheet} from "../../resources/styles";
+
+
+export interface ICategoryCardState {}
+export interface ICategoryCardProps {
+  size: number,
+  name: string,
+  image?: any,
+  style?: any,
+}
+
+class CategoryCard extends Component<Readonly<ICategoryCardProps>, Readonly<ICategoryCardState>> {
+  constructor(props: any) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const {
+      size,
+      name,
+      image,
+      style,
+    } = this.props
+    return (
+      <View
+        style={{
+          width: size,
+          ...stylesheet.categoriesScreenCard,
+          ...style,
+        }}
+      >
+        <Image
+          source={require("../../resources/assets/drawable/food.jpg")}
+          style={{
+            width: size,
+            height: size,
+            borderRadius: 10,
+          }}
+        />
+        <View style={stylesheet.categoriesScreenCardTitleContainer}>
+          <Text style={stylesheet.categoriesScreenCardTitle}>{name}</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+export default CategoryCard;
--- src/screens/CategoriesScreen.tsx
+++ src/screens/CategoriesScreen.tsx
@@ -1,20 +1,72 @@
 import React, {Component} from "react";
-import {Text, View} from "react-native";
+import {Dimensions, FlatList, Image, ImageBackground, Text, View} from "react-native";
 import {stylesheet} from "../../resources/styles";
+import FishIcon from "../../resources/assets/drawable/fish_back_button.svg"
+import CategoryCard from "../components/CategoryCard";
 
-export interface ICategoriesScreenState {}
 
-class CategoriesScreen extends Component<Readonly<any>, Readonly<ICategoriesScreenState>> {
+export interface ICategoriesScreenState {
+  mainContainerWidth: number,
+  cardsInfo: any[],
+}
+
+class CategoriesScreen extends Component<
+  Readonly<any>,
+  Readonly<ICategoriesScreenState>
+  > {
   constructor(props: any) {
     super(props);
-    this.state = {};
+    this.state = {
+      mainContainerWidth: Dimensions.get("window").width,
+      cardsInfo: [
+        {
+          id: 0,
+          name: "Поке от шефа",
+        },
+        {
+          id: 1,
+          name: "Супы",
+        },
+        {
+          id: 2,
+          name: "Роллы",
+        },
+        {
+          id: 4,
+          name: "Вок",
+        },
+      ]
+    };
   }
 
   render() {
+    const renderItem = ({ item, index }) => (
+      <CategoryCard
+        size={(this.state.mainContainerWidth -2*stylesheet.categoriesScreenContainer.paddingHorizontal - 10)/2}
+        name={item.name}
+      />
+    );
     return (
-      <View style={stylesheet.centerBody}>
-        <Text>Categories Screen</Text>
-      </View>
+      <ImageBackground
+        source={require("../../resources/assets/drawable/background.png")}
+        style={{flex: 1}}
+      >
+        <View style={stylesheet.categoriesScreenContainer}>
+          <View style={stylesheet.categoriesScreenHeaderContainer}>
+            <View style={stylesheet.categoriesScreenHeader}>
+              <FishIcon width={47} height={17} style={stylesheet.headerFishBackButton}/>
+              <Text style={stylesheet.categoriesScreenHeaderText}>Разделы</Text>
+            </View>
+          </View>
+
+          <FlatList
+            data={this.state.cardsInfo}
+            renderItem={renderItem}
+            keyExtractor={item => item.id}
+            numColumns={2}
+          />
+        </View>
+      </ImageBackground>
     );
   }
 }
