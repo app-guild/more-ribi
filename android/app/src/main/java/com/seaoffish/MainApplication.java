962918e2e9df1e0239ae0f26e94eb9b09d6cd3bf
stormkirill98 <stormkirill98@gmail.com>
2020-09-09 21:33:23 +0300

    добавил бд в проект, подключил библиотеку и открыл бд для работы при запуске приложения

--- android/app/src/main/java/com/seaoffish/MainApplication.java
+++ android/app/src/main/java/com/seaoffish/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import io.liteglue.SQLitePluginPackage;
 import com.horcrux.svg.SvgPackage;
 import com.reactnativepayments.ReactNativePaymentsPackage;
 import com.facebook.react.ReactInstanceManager;
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import "react-native-gesture-handler";
 import * as React from "react";
-import { useEffect } from 'react';
+import {useEffect} from 'react';
 import SplashScreen from 'react-native-splash-screen';
 import Navigation from "./components/Navigation";
+import SQLite from "react-native-sqlite-storage";
 
 export default function App() {
     useEffect(() => {
         SplashScreen.hide();
     }, []);
-    
     return <Navigation />;
 }
+
+global.db = SQLite.openDatabase(
+    {
+        name: "SQLite",
+        location: "default",
+        createFromLocation: "~SQLite.db",
+    },
+    () => console.log("SUCCESS OPEN DB"),
+    (error: any) => console.error("ERROR: " + error),
+);
