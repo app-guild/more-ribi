f043641fd055b99afc716093b0b0f38438bb769a
Mikhail Suendukov <43934531+JiglioNero@users.noreply.github.com>
2020-09-17 17:07:13 +0300

    implement google pay

--- src/screens/FeedbackScreen.tsx
+++ src/screens/FeedbackScreen.tsx
@@ -1,22 +1,40 @@
 import React, {Component} from "react";
-import {Text, View} from "react-native";
+import {Text, TouchableOpacity, View} from "react-native";
 import {globalStylesheet} from "../../resources/styles";
+import {Currency} from "../utils/payment/Currency";
 
 export interface IFeedbackScreenState {}
 
-class FeedbackScreen extends Component<
-    Readonly<any>,
-    Readonly<IFeedbackScreenState>
-> {
+class FeedbackScreen extends Component<Readonly<any>, Readonly<IFeedbackScreenState>> {
     constructor(props: any) {
         super(props);
         this.state = {};
+    }
+
+    payWithGooglePay(): void {
+        global.googlePayService
+            .doPaymentRequest(
+                {
+                    totalPrice: "10",
+                    totalPriceStatus: "FINAL",
+                    currencyCode: Currency.USD,
+                },
+                (token: string) => console.log(token),
+            )
+            .then(() => {
+                return;
+            });
     }
 
     render() {
         return (
             <View style={globalStylesheet.centerBody}>
                 <Text>Feedback Screen</Text>
+
+                <Text>Welcome to react-native-google-pay!</Text>
+                <TouchableOpacity onPress={() => this.payWithGooglePay()}>
+                    <Text>PAYMENT_GATEWAY</Text>
+                </TouchableOpacity>
             </View>
         );
     }
---
+++ src/utils/payment/GooglePayService.ts
@@ -0,0 +1,92 @@
+import {AllowedCardAuthMethodsType, AllowedCardNetworkType, GooglePay} from "react-native-google-pay";
+import {Currency} from "./Currency";
+
+export interface IPaymentTransaction {
+    totalPrice: string;
+    totalPriceStatus: "FINAL" | "NOT_CURRENTLY_KNOWN";
+    currencyCode: Currency;
+}
+
+export interface IGateway {
+    type: "PAYMENT_GATEWAY";
+
+    gateway: string;
+    gatewayMerchantId?: string;
+
+    // Add object for identify gateway
+}
+
+export interface IDirect {
+    type: "DIRECT";
+    publicKey: string;
+}
+
+export interface IPaymentMethod {
+    tokenizationSpecification: IGateway | IDirect;
+    allowedCardNetworks: AllowedCardNetworkType[];
+    allowedCardAuthMethods: AllowedCardAuthMethodsType[];
+}
+
+export interface ICardPaymentRequest {
+    cardPaymentMethod: IPaymentMethod;
+    transaction: IPaymentTransaction;
+    merchantName: string;
+}
+
+export class GooglePayService {
+    public cardPaymentMethodMap: IPaymentMethod;
+    public merchantName: string;
+
+    constructor(
+        cardPaymentMethodMap: IPaymentMethod,
+        merchantName: string,
+        environment?: "ENVIRONMENT_TEST" | "ENVIRONMENT_PRODUCTION",
+    ) {
+        this.cardPaymentMethodMap = cardPaymentMethodMap;
+        this.merchantName = merchantName;
+        switch (environment) {
+            case "ENVIRONMENT_PRODUCTION":
+                GooglePay.setEnvironment(GooglePay.ENVIRONMENT_PRODUCTION);
+                break;
+            case "ENVIRONMENT_TEST":
+            default:
+                GooglePay.setEnvironment(GooglePay.ENVIRONMENT_TEST);
+        }
+    }
+
+    public setTestEnvironment() {
+        GooglePay.setEnvironment(GooglePay.ENVIRONMENT_TEST);
+    }
+
+    public setProductionEnvironment() {
+        GooglePay.setEnvironment(GooglePay.ENVIRONMENT_PRODUCTION);
+    }
+
+    public async doPaymentRequest(
+        transaction: IPaymentTransaction,
+        tokenCallback?: (token: string) => void,
+    ): Promise<void> {
+        const paymentRequest: ICardPaymentRequest = {
+            cardPaymentMethod: this.cardPaymentMethodMap,
+            merchantName: this.merchantName,
+            transaction: transaction,
+        };
+
+        return GooglePay.isReadyToPay(
+            paymentRequest.cardPaymentMethod.allowedCardNetworks,
+            paymentRequest.cardPaymentMethod.allowedCardAuthMethods,
+        ).then((ready) => {
+            if (ready) {
+                // Request payment token
+                GooglePay.requestPayment(paymentRequest)
+                    .then((token: string) => {
+                        // Send a token to your payment gateway
+                        if (tokenCallback) {
+                            tokenCallback(token);
+                        }
+                    })
+                    .catch((error) => console.log(error.code, error.message));
+            }
+        });
+    }
+}
