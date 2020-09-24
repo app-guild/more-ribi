255f8ef748e53a2ae420bea0ee7979e95f377653
DobrolyubovMaxim <56647828+DobrolyubovMaxim@users.noreply.github.com>
2020-09-25 00:53:46 +0300

    Open dish (#35)
    * scroll fix
    
    * open dish class
    
    * dopilivanie
    
    * commit
    
    * use Modal component from react-native
    
    * use Modal component from react-native
    
    * invisible listView when modal visible
    
    * rename globalColors, remove comments
    
    * e
    
    * implement some TODO in MainScreen
    
    * new Animation, reduce size
    
    * add window close onBackdropPress & onBackButtonPress
    
    * adaptation fix
    
    * fix product card size calculation
    
    * set 2 line text for product name
    
    * fix
    
    * remove needCategoryName prop from header
    
    * refactor
    
    * fixes
    
    * optimise cart refreshing
    
    * some fixes
    
    * fix cart control with database
    
    Co-authored-by: JiglioNero <smouk.chayz@gmail.com>
    Co-authored-by: Mikhail Suendukov <43934531+JiglioNero@users.noreply.github.com>

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,85 +1,137 @@
 import React, {Component} from "react";
 import {Image, Text, View, TouchableOpacity, StyleSheet} from "react-native";
 import {globalColors, globalStylesheet} from "../../resources/styles";
-import CardIcon from "../../resources/assets/drawable/cart_icon.svg";
-import DatabaseApi, {TKey} from "../database/DatabaseApi";
+import DatabaseApi from "../utils/database/DatabaseApi";
 import Product from "../entities/Product";
+import Cart from "../entities/Cart";
 
-export interface IProductCardState {}
+export interface IProductCardState {
+    product: Product;
+    countInCart: number;
+}
 
 export interface IProductCardProps {
-    width: number;
-    height: number;
     product: Product;
-    style?: any;
+    countInCart: number;
+    onClick: (product: Product) => any;
 }
 
 class ProductCard extends Component<Readonly<IProductCardProps>, Readonly<IProductCardState>> {
-    constructor(props: any) {
+    constructor(props: IProductCardProps) {
         super(props);
-        this.state = {};
+        this.state = {
+            product: props.product,
+            countInCart: props.countInCart,
+        };
+        this.addToCart = this.addToCart.bind(this);
+        this.getCountInCart = this.getCountInCart.bind(this);
     }
 
-    private addToCart(productId: TKey) {
-        // TODO notify to MainScreen new cart price
-        DatabaseApi.addProductToCart(productId);
+    componentDidMount() {
+        DatabaseApi.addOnCartChangeListener(this.getCountInCart);
+        return DatabaseApi.getLastCart().then(this.getCountInCart);
+    }
+
+    componentWillUnmount() {
+        DatabaseApi.removeOnCartChangeListener(this.getCountInCart);
+    }
+
+    private getCountInCart(cart: Cart) {
+        console.log("Products count: " + cart.products.length);
+        const thisProducts = cart.products.filter((prod) => prod.id === this.state.product.id);
+        this.setState({countInCart: thisProducts.length});
+    }
+
+    private async addToCart() {
+        if (this.state.countInCart === 0) {
+            return DatabaseApi.addProductToCart(this.props.product.id);
+        } else {
+            return DatabaseApi.updateProductCount(this.props.product.id, this.state.countInCart + 1);
+        }
+    }
+
+    private renderPrice(price: number, discountPrice?: number | null) {
+        let result: any;
+        let styleForMainPrice = stylesheet.shoppingCartMainPrice;
+        if (this.state.countInCart > 0) {
+            styleForMainPrice = stylesheet.shoppingCartMainPriceSelected;
+        }
+        let displayCountOfSelected = "";
+        if (this.state.countInCart > 1) {
+            displayCountOfSelected = `  X ${this.state.countInCart}`;
+        }
+
+        if (discountPrice || discountPrice === 0) {
+            let mainPriceTextColor = globalColors.headerUnderlineColor;
+            if (this.state.countInCart > 0) {
+                mainPriceTextColor = globalColors.backgroundOverlay;
+            }
+            result = (
+                <View style={stylesheet.shoppingCartPriceContainer}>
+                    <Text
+                        numberOfLines={1}
+                        style={{
+                            ...globalStylesheet.crossedOutPrice,
+                            marginLeft: stylesheet.shoppingCartMainPrice.padding,
+                        }}>
+                        {price + " ₽"}
+                    </Text>
+                    <View style={{display: "flex", flexDirection: "row"}}>
+                        <View style={styleForMainPrice}>
+                            <Text numberOfLines={1} style={{...globalStylesheet.price, color: mainPriceTextColor}}>
+                                {discountPrice + " ₽" + displayCountOfSelected}
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+            );
+        } else {
+            let mainPriceTextColor = globalColors.crossedOutPriceColor;
+            if (this.state.countInCart > 0) {
+                mainPriceTextColor = globalColors.backgroundOverlay;
+            }
+            result = (
+                <View style={stylesheet.shoppingCartPriceContainer}>
+                    <View style={{display: "flex", flexDirection: "row"}}>
+                        <View style={styleForMainPrice}>
+                            <Text numberOfLines={1} style={{...globalStylesheet.price, color: mainPriceTextColor}}>
+                                {price + " ₽" + displayCountOfSelected}
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+            );
+        }
+        return result;
     }
 
     render() {
-        const {width, height, product, style} = this.props;
+        const {product, onClick} = this.props;
+        const image = product.imageUrl ? {uri: product.imageUrl} : require("../../resources/assets/drawable/food.jpg");
 
         return (
-            <View
-                style={{
-                    width: width,
-                    height: height,
-                    ...stylesheet.container,
-                    ...style,
-                }}>
-                <Image
-                    source={require("../../resources/assets/drawable/food.jpg")}
-                    style={{
-                        width: width - 2 * stylesheet.container.padding,
-                        height: (width - 2 * stylesheet.container.padding) / 1.2,
-                        borderRadius: 20,
-                    }}
-                />
+            <View style={stylesheet.container}>
                 <View
-                    style={{
-                        flexDirection: "row",
-                        alignItems: "flex-end",
-                        justifyContent: "space-between",
-                        marginTop: 2,
+                    style={{flexDirection: "row"}}
+                    onTouchEnd={() => {
+                        onClick(product);
                     }}>
-                    <View style={{maxWidth: 100, justifyContent: "flex-end"}}>
-                        <Text
-                            numberOfLines={1}
-                            style={{
-                                marginTop: 5,
-                                ...globalStylesheet.primaryText,
-                            }}>
+                    <View style={stylesheet.shoppingCartImageContainer}>
+                        <Image source={image} style={stylesheet.shoppingCartImage} />
+                    </View>
+                    <View style={stylesheet.shoppingCardTextContainer}>
+                        <Text numberOfLines={2} style={globalStylesheet.primaryText}>
                             {product.name}
                         </Text>
-                        <Text numberOfLines={1} style={globalStylesheet.crossedOutPrice}>
-                            {product.discountPrice}
-                        </Text>
-                        <Text numberOfLines={1} style={globalStylesheet.price}>
-                            {product.price}
+                        <Text numberOfLines={3} style={{...globalStylesheet.secondaryText, marginTop: 10}}>
+                            {product.composition}
                         </Text>
                     </View>
-                    <View style={stylesheet.shoppingCartButtonContainer}>
-                        <TouchableOpacity
-                            style={stylesheet.shoppingCartButton}
-                            activeOpacity={0.85}
-                            onPress={() => this.addToCart(product.id)}>
-                            <CardIcon
-                                width={20}
-                                height={20}
-                                fill={globalColors.cardBackgroundColor}
-                                style={{position: "absolute"}}
-                            />
-                        </TouchableOpacity>
-                    </View>
+                </View>
+                <View style={stylesheet.shoppingCartButtonContainer}>
+                    <TouchableOpacity activeOpacity={0.85} onPress={this.addToCart}>
+                        {this.renderPrice(product.price, product.discountPrice)}
+                    </TouchableOpacity>
                 </View>
             </View>
         );
@@ -88,31 +140,62 @@ class ProductCard extends Component<Readonly<IProductCardProps>, Readonly<IProdu
 
 export const stylesheet = StyleSheet.create({
     container: {
-        borderRadius: 20,
+        borderWidth: 1,
+        borderColor: globalColors.transparent,
+        borderBottomColor: globalColors.accentColor,
+        marginHorizontal: 10,
         padding: 10,
-        backgroundColor: globalColors.cardBackgroundColor,
-
-        shadowColor: globalColors.shadowColor,
-        shadowOffset: {
-            width: 0,
-            height: 1,
-        },
-        shadowOpacity: 0.2,
-        shadowRadius: 1.41,
-        elevation: 2,
+        justifyContent: "space-between",
+        flexDirection: "row",
+        width: "95%",
+        height: "100%",
     },
-    shoppingCartButton: {
-        justifyContent: "center",
-        alignItems: "center",
-        borderRadius: 50,
-        backgroundColor: globalColors.primaryColor,
-        width: 38,
-        height: 38,
-        opacity: 0.5,
+    shoppingCardTextContainer: {
+        marginHorizontal: 10,
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        width: "30%",
     },
     shoppingCartButtonContainer: {
         justifyContent: "center",
+        alignItems: "flex-end",
+        width: "30%",
+    },
+    shoppingCartImageContainer: {
+        justifyContent: "center",
         alignItems: "center",
+        height: "100%",
+    },
+    shoppingCartImage: {
+        height: "100%",
+        aspectRatio: 1,
+        borderRadius: 20,
+    },
+    shoppingCartPriceContainer: {
+        marginRight: 10,
+        flexDirection: "column",
+        justifyContent: "flex-end",
+        alignItems: "center",
+        width: "100%",
+    },
+    shoppingCartMainPrice: {
+        borderColor: globalColors.accentColor,
+        borderWidth: 1,
+        borderTopLeftRadius: 10,
+        borderBottomRightRadius: 10,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 5,
+        width: "100%",
+    },
+    shoppingCartMainPriceSelected: {
+        backgroundColor: globalColors.headerUnderlineColor,
+        borderTopLeftRadius: 10,
+        borderBottomRightRadius: 10,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 5,
+        width: "100%",
     },
 });
 
