bc7bd0c015f1dec95b8b5208f5852aee73792b25
Kirill Panihin <39589891+stormkirill98@users.noreply.github.com>
2020-10-03 11:10:17 +0300

    delivery terms (#55)
    * delivery terms
    
    * удалил ненужный импорт
    
    Co-authored-by: ka.panihin <ka.panihin@tensor.ru>

--- src/screens/DeliveryConditionsScreen.tsx
+++ src/screens/DeliveryConditionsScreen.tsx
@@ -1,10 +1,10 @@
 import React, {Component} from "react";
-import {Text, View} from "react-native";
-import {globalStylesheet} from "../../resources/styles";
+import {StyleSheet, Text, View} from "react-native";
+import {Divider} from "react-native-paper";
 
 export interface IDeliveryConditionsScreenState {}
 
-class DeliveryConditionsScreen extends Component<
+export default class DeliveryConditionsScreen extends Component<
     Readonly<any>,
     Readonly<IDeliveryConditionsScreenState>
 > {
@@ -15,11 +15,49 @@ class DeliveryConditionsScreen extends Component<
 
     render() {
         return (
-            <View style={globalStylesheet.centerBody}>
-                <Text>Delivery Conditions Screen</Text>
+            <View style={stylesheet.container}>
+                <Divider style={stylesheet.divider} />
+                <Text style={stylesheet.indent}>Бесплатная доставка еды по Ярославлю при заказе от 500 Р</Text>
+
+                <Text style={stylesheet.bold}>Время работы:</Text>
+                <Text>
+                    Ярославль, Некрасова 52/35: <Text style={stylesheet.underline}>09:00-22:00</Text>
+                </Text>
+                <Text>
+                    Ярославль, Свободы 52: <Text style={stylesheet.underline}>09:00-22:00</Text>
+                </Text>
+                <Text style={stylesheet.indent}>
+                    Ярославль, Комсомольская 12: <Text style={stylesheet.underline}>09:00-00:00</Text>
+                </Text>
+
+                <Text style={{...stylesheet.indent, ...stylesheet.bold}}>Заказы принимаются с 09:00 до 00:00</Text>
+
+                <Text style={stylesheet.bold}>Время доставки</Text>
+                <Text style={stylesheet.indent}>35-60 минут</Text>
+
+                <Text style={stylesheet.bold}>Оплата:</Text>
+                <Text>Наличными курьеру, банковской картой в приложении, картой курьеру</Text>
             </View>
         );
     }
 }
 
-export default DeliveryConditionsScreen;
+const stylesheet = StyleSheet.create({
+    container: {
+        paddingHorizontal: 20,
+    },
+    divider: {
+        marginTop: 5,
+        marginBottom: 20,
+        height: 1,
+    },
+    underline: {
+        textDecorationLine: "underline",
+    },
+    bold: {
+        fontWeight: "bold",
+    },
+    indent: {
+        marginBottom: 30,
+    },
+});
